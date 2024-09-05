b94ea114dd5e8913858838032f1e5cd8e08dd115
AdamKaczmarski <adam.kacz10@gmail.com>
2024-09-05 19:46:24 +0100

    leet dailies

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,16 +1,11 @@
 import solutions.Solution;
-import solutions.leetcode.arrays.LC_1894;
-import solutions.leetcode.arrays.LC_1945;
-import solutions.leetcode.arrays.LC_874;
-import solutions.neetcode.roadmap.dynamic_programming_2d.*;
-import solutions.neetcode.roadmap.graphs.LC_323;
-import solutions.neetcode.roadmap.graphs.LC_743;
+import solutions.leetcode.arrays.LC_2028;
 
 
 public class Main {
     public static void main(String[] args) {
         Solution solution;
-        solution = new LC_874();
+        solution = new LC_2028();
         solution.solve();
     }
 }
