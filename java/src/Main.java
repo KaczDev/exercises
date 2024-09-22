4f32c08abeddad6613ff79f27fcf8e822166394b
AdamKaczmarski <adam.kacz10@gmail.com>
2024-09-22 22:18:30 +0100

    LC Daily LC_440

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,6 +2,7 @@ import solutions.Solution;
 import solutions.leetcode.arrays.LC_1371;
 import solutions.leetcode.arrays.LC_539;
 import solutions.leetcode.backtracking.LC_386;
+import solutions.leetcode.backtracking.LC_440;
 import solutions.leetcode.bitmanipulation.LC_2419;
 import solutions.leetcode.dynamic_programming.LC_241;
 import solutions.leetcode.trees.LC_222;
@@ -10,7 +11,7 @@ import solutions.leetcode.trees.LC_222;
 public class Main {
     public static void main(String[] args) {
         Solution solution;
-        solution = new LC_386();
+        solution = new LC_440();
         solution.solve();
     }
 }
